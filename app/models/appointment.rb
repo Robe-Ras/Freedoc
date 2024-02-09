425c3861c79ff43e5a36ce964c78846a99acc4df
Robena Rasolond <82022919+Robe-Ras@users.noreply.github.com>
2024-02-09 04:11:34 +0100

    Delete app directory

--- app/models/appointment.rb
+++
@@ -1,6 +0,0 @@
-class Appointment < ApplicationRecord
-  belongs_to :doctor
-  belongs_to :patient
-  belongs_to :city
-  belongs_to :specialty
-end
--- app/models/specialty.rb
+++
@@ -1,4 +0,0 @@
-class Specialty < ApplicationRecord
-  has_and_belongs_to_many :doctors 
-end
-
